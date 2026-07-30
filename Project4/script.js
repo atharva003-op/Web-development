dbc14c0078ccfb5747c1be6697bb5090300c2c52
Atharva Haibatti <aatharva357@gmail.com>
2026-07-30 18:25:22 +0530

    Add image change functionality with buttons

---
+++ Project4/script.js
@@ -0,0 +1,14 @@
+document.getElementById("btn1").onclick = function() {
+    document.getElementById("img1").src = "batman_img2.jpg";
+    document.getElementById("img2").src = "superman_img2.jpg";
+}
+
+document.getElementById("btn2").onclick = function() {
+    document.getElementById("img1").src = "batman_gif.gif";
+    document.getElementById("img2").src = "superman_gif.gif";
+}
+
+document.getElementById("btn3").onclick = function() {
+    document.getElementById("img1").src = "batman_img1.jpg";
+    document.getElementById("img2").src = "superman_img1.jpg";
+}
